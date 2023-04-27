5630f22c124f329da4c520ee5e42aac2918f5c33
bywillsilva <raniwillere@gmail.com>
2023-04-27 13:28:51 -0300

    Atualizando interface mobile e adicionando informações

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const grid = document.querySelector("#grid");
 const menu = document.querySelector("#menu");
-const title = document.querySelector("#title");
+const title = document.querySelector(".title");
+const mainTitle = document.querySelector("#mainTitle");
+const mainText = document.querySelector("#mainText");
 
-grid.addEventListener("click", () => {
+const home = document.querySelector("#home");
+home.addEventListener("click", () => {
+    window.location.reload();
+});
+
+const sobre = document.querySelector("#sobre");
+sobre.addEventListener("click", () => {
+    checkGrid();
+    mainTitle.innerHTML = "Sobre mim";
+    mainText.classList.add("p");
+    mainText.innerHTML =
+        "Olá! Me chamo Willian como você já sabe mas pode me chamar de Will.<br> Iniciei minha carreira dev em 2019 e desde então não parei mais, no decorrer desse tempo já desenvolvi muitos projetos com a minha linguagem preferida, JavaScript.";
+});
+
+const checkGrid = () => {
     let onDisplay = window.getComputedStyle(menu).display;
     if (onDisplay == "none") {
         menu.classList.remove("none");
@@ -15,6 +31,10 @@ grid.addEventListener("click", () => {
         menu.classList.remove("center");
         grid.src = "./icons/grid-icon.svg";
     }
+};
+
+grid.addEventListener("click", () => {
+    checkGrid();
 });
 
 title.addEventListener("click", () => {
