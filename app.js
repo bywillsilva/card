8718df7f6d6e3c94c24ff056cc5111357711b3d8
bywillsilva <raniwillere@gmail.com>
2023-04-27 16:09:18 -0300

    Atualizando informações e melhorando o desempenho

--- app.js
+++ app.js
@@ -15,7 +15,23 @@ sobre.addEventListener("click", () => {
     mainTitle.innerHTML = "Sobre mim";
     mainText.classList.add("p");
     mainText.innerHTML =
-        "Olá! Me chamo Willian como você já sabe mas pode me chamar de Will.<br> Iniciei minha carreira dev em 2019 e desde então não parei mais, no decorrer desse tempo já desenvolvi muitos projetos com a minha linguagem preferida, JavaScript.";
+        "Olá! Me chamo Willian como você já sabe mas pode me chamar de Will.<br> Iniciei minha carreira dev em 2019 e desde então não parei mais. No decorrer desse tempo já desenvolvi muitos projetos com a minha linguagem preferida, JavaScript.";
+});
+
+const skills = document.querySelector("#skills");
+skills.addEventListener("click", () => {
+    checkGrid();
+    mainTitle.innerHTML = "Skills";
+    mainText.classList.add("p");
+    mainText.innerHTML = "Tenho muito conhecimento no desenvolvimento com JavaScript, Node, React, Html e Css, além de outras tecnologias que tenho estudado para o desenvolvimento fullstack.";
+});
+
+const contato = document.querySelector("#contato");
+contato.addEventListener("click", () => {
+    checkGrid();
+    mainTitle.innerHTML = "Contato";
+    mainText.classList.add("p");
+    mainText.innerHTML = "willian.raniere@icloud.com <br> @bywill_silva <br> (84) 9 8604-6531";
 });
 
 const checkGrid = () => {
